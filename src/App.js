2cac038fb4bd1ebc7900b36f2ab18ac23fae872a
hsggamer <55681300+hsggamer@users.noreply.github.com>
2022-12-04 15:24:57 +0400

    User Pages Update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import AbuDhabi from "./components/AbuDhabi/index";
 import Ajman from "./components/Ajman/index";
 import Dubai from "./components/Dubai/index";
 import Sharjah from "./components/Sharjah/index";
+import UserAbuDhabi from './components/UserAbuDhabi';
+import UserAjman from './components/UserAjman';
+import UserDubai from './components/UserDubai';
+import UserSharjah from './components/UserSharjah';
+import UserHome from './components/UserHome/Home';
+import TimeSlot from './components/TimeSlot';
 // import Success from "./components/Success/index";
 // import UserPage from './components/UserPage/index';
 
@@ -21,6 +27,12 @@ const App = () => {
           <Route path="/Ajman" element={<Ajman />} />
           <Route path="/Dubai" element={<Dubai />} />
           <Route path="/Sharjah" element={<Sharjah />} />
+          <Route path="/UserHome" element={<UserHome />} />
+          <Route path="/UserAbuDhabi" element={<UserAbuDhabi />} />
+          <Route path="/UserAjman" element={<UserAjman />} />
+          <Route path="/UserDubai" element={<UserDubai />} />
+          <Route path="/UserSharjah" element={<UserSharjah />} />
+          <Route path="/User/:id" element={<TimeSlot />} />
           {/* <Route path="/Success" element={<Success />} />
           <Route path="/user/:user_id" element={<UserPage />} /> 
           <Route path="*" element={<Errorpage />} /> */}
