e8cef99db6e594e480aa9c7f877906c20db2a1d4
hsggamer <55681300+hsggamer@users.noreply.github.com>
2023-01-03 11:19:03 +0400

    Update index.js

--- src/components/UserInfiniti/index.js
+++ src/components/UserInfiniti/index.js
@@ -77,19 +77,19 @@ var count = 0
     }
 
     function carClick(e){
-        if(e === 'QX80'){
-            document.getElementById(e).style.background = "rgb(200, 197, 197)";
-            document.getElementById('QX60').style.background = "transparent";
-            document.getElementById('QX55').style.background = "transparent";
-            // document.getElementById('Q60').style.background = "transparent";
-            document.getElementById('Q50').style.background = "transparent";
-            document.getElementById('QX50').style.background = "transparent";
-            setCar('/QX80');
-        }
+        // if(e === 'QX80'){
+        //     document.getElementById(e).style.background = "rgb(200, 197, 197)";
+        //     document.getElementById('QX60').style.background = "transparent";
+        //     document.getElementById('QX55').style.background = "transparent";
+        //     // document.getElementById('Q60').style.background = "transparent";
+        //     document.getElementById('Q50').style.background = "transparent";
+        //     document.getElementById('QX50').style.background = "transparent";
+        //     setCar('/QX80');
+        // }
 
         if(e === 'QX60'){
             document.getElementById(e).style.background = "rgb(200, 197, 197)";
-            document.getElementById('QX80').style.background = "transparent";
+            // document.getElementById('QX80').style.background = "transparent";
             document.getElementById('QX55').style.background = "transparent";
             // document.getElementById('Q60').style.background = "transparent";
             document.getElementById('Q50').style.background = "transparent";
@@ -99,7 +99,7 @@ var count = 0
 
         if(e === 'QX55'){
             document.getElementById(e).style.background = "rgb(200, 197, 197)";
-            document.getElementById('QX80').style.background = "transparent";
+            // document.getElementById('QX80').style.background = "transparent";
             document.getElementById('QX60').style.background = "transparent";
             // document.getElementById('Q60').style.background = "transparent";
             document.getElementById('Q50').style.background = "transparent";
@@ -119,7 +119,7 @@ var count = 0
 
         if(e === 'Q50'){
             document.getElementById(e).style.background = "rgb(200, 197, 197)";
-            document.getElementById('QX80').style.background = "transparent";
+            // document.getElementById('QX80').style.background = "transparent";
             document.getElementById('QX60').style.background = "transparent";
             document.getElementById('QX55').style.background = "transparent";
             // document.getElementById('Q60').style.background = "transparent";
@@ -129,7 +129,7 @@ var count = 0
 
         if(e === 'QX50'){
             document.getElementById(e).style.background = "rgb(200, 197, 197)";
-            document.getElementById('QX80').style.background = "transparent";
+            // document.getElementById('QX80').style.background = "transparent";
             document.getElementById('QX60').style.background = "transparent";
             document.getElementById('QX55').style.background = "transparent";
             // document.getElementById('Q60').style.background = "transparent";
@@ -166,12 +166,12 @@ var count = 0
 
             <div className="rower" style={{display: 'flex', textAlign: 'center', justifyContent: 'center', alignItems: 'center', margin: '0', padding:'0'}} >
     
-                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end'}}>
+                {/* <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end'}}>
                     <div id='QX80' className="content content-1" onClick={()=>{carClick('QX80')}} style={{padding: '0', margin: '10px'}}>
                         <img src={QX80} alt="Logo" style={{width: '20vh'}}/>
                         <p style={{fontSize: '10px'}}>QX80</p>
                     </div>
-                </div>
+                </div> */}
 
                 <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start'}}>
                     <div id='QX60' className="content content-1" onClick={()=>{carClick('QX60')}} style={{padding: '0', margin: '10px'}}>
